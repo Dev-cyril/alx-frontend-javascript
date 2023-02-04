470aaa94415e7d28150b3fa52849c5968efa96d3
Cyril <cyril.idu2@gmail.com>
2023-02-04 02:12:58 +0000

    eslint observation

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
-export default function updateUniqueItems (map) {
-	 if (list instanceof Map){
-		 for (const [key, value] of list) {
-			           if (value === 1) {
-					           list.set(key, 100);
-					         }
-			         }
-		   } else {
-			       throw new Error('Cannot process');
-			     }
-	  return list;
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    for (const [key, value] of map) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    }
+  } else {
+    throw new Error('Cannot process');
+  }
+  return map;
 }
