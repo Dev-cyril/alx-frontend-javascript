fa57220f32f05951b9dc12c7c2ecfc5c14d2541d
Cyril <cyril.idu2@gmail.com>
2023-07-09 16:03:19 +0100

    updates

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,10 @@
 export default function getResponseFromAPI(){
-    const promise = new Promise(() =>{})
+    const promise = new Promise((resolve, reject) =>{
+        if (true){
+            resolve();
+        } else{
+            reject();
+        }
+    })
     return promise
 }
